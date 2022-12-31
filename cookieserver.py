3be8b858544e89eadfec18a08fa23c856731bdbb
Parchaks <parchaks@protonmail.com>
2022-12-30 15:48:09 -0900

    adding server

---
+++ cookieserver.py
@@ -0,0 +1,22 @@
+#!/bin/python
+#Modifed SimpleHTTPServer which returns cookies passed on the request
+
+from http import server 
+from http.cookies import SimpleCookie
+import socketserver
+
+port = 1234
+
+class MyHTTPRequestHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        cookies = SimpleCookie(self.headers.get('Cookie'))
+#        print(f'{self.headers["Host"]} connected')
+        with open('server.log','a') as f:
+            f.write(f"\n {self.headers['Host']} : {cookies} \n")
+        self.send_response(200)
+        self.end_headers()
+
+if __name__ == '__main__':
+    with socketserver.TCPServer(('',port), MyHTTPRequestHandler) as httpd:
+        print(f'Starting server at port {port}, use <Ctrl-C> to stop')
+        httpd.serve_forever()
